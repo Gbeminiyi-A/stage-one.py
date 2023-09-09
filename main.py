93b4d455af21ffdf47c552482109983c53175e3a
Oluwagbeminiyi <gagbedejobi@gmail.com>
2023-09-09 22:54:15 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+from datetime import date
+import datetime
+
+app = Flask(__name__)
+
+day = date.today().strftime("%A")
+
+
+@app.route("/")
+def my_api():
+
+    name = request.args.get("slack_name")
+    track = request.args.get("track")
+
+    return jsonify(
+        slack_name=name,
+        current_day=day,
+        utc_time=datetime.datetime.utcnow().isoformat() + "Z",
+        track=track,
+        github_file_url="https://github.com/username/repo/blob/main/file_name.ext",
+        github_repo_url="https://github.com/username/repo",
+        status_code=200
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
